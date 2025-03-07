97ac65342df3ccf8211b8492e212f123bda2ec35
kranpant <110833432+kranpant@users.noreply.github.com>
2025-03-07 12:35:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","27383453"))
-API_HASH = getenv("API_HASH","4c246fb0c649477cc2e79b6a178ddfaa")
+API_ID = int(getenv("API_ID",""))
+API_HASH = getenv("API_HASH","")
 
-BOT_TOKEN = getenv("BOT_TOKEN","6878567316:AAFNWjNAk7-KyJRsqD_1mKSpc3yF-T4B-a8")
-OWNER_ID = int(getenv("OWNER_ID","6474527080"))
+BOT_TOKEN = getenv("BOT_TOKEN","")
+OWNER_ID = int(getenv("OWNER_ID",""))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI","")
 MUST_JOIN = getenv("MUST_JOIN","")
